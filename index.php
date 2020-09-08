126429f5c25dfae8d5c8710c4681e38f9a10c915
Kwynn <buessresume@gmail.com>
2020-09-07 23:27:21 -0400

    email version ready or almost so

--- index.php
+++ index.php
@@ -1,62 +1,24 @@
 <?php
 
-require_once('/opt/kwynn/kwutils.php');
+require_once('/opt/kwynn/kwcod.php');
+require_once('get.php');
+require_once('/opt/kwynn/email.php');
 
-dci_2021_1::get();
-
-class dci_2021_1 {
-    
-    const liveURL   = 'https://www.dci.org/';
-    const localTempFilePath = '/tmp/dci_2020_1_1.html';
-    
-    public static function get() {
-	$ret = self::mget();
-	self::pop2021Eval($ret);
-	return $ret;
+doit();
+function doit() {
+    if (isAWS() && time() > strtotime('2020-09-08 02:00')) sleep(random_int(0, 5400));
+    $r = dci_2021_1::get();
+    if ($r['is21']) {
+	$sub  = 'DCI schedule change!';
+	$body = 'is21 = true';
+    } else if (date('D') === 'Mon') {
+	$sub  = 'schedule ping';
+	$body = print_r($r, 1);
+	$len  = strlen($body);
+	kwas($len < 2000, 'res / body too big - 1116pm');
     }
-  
-    private static function mget() {
 
-	if (!isAWS() &&   
-	    file_exists(self::localTempFilePath))
-	         $url = self::localTempFilePath;
-	else     $url = self::liveURL;
-	
-	$res = file_get_contents($url);
-
-	$b = microtime(1);
-	$res  = file_get_contents($url);	
-	$e = microtime(1);
-	unset($http_response_header);
-	$callElapsed = $e - $b;
-	
-	if ($url ===	     self::localTempFilePath) 
-	     $ts = filemtime(self::localTempFilePath);
-	else $ts = intval($e); unset($b, $e);
-	$len = strlen($res);
-	file_put_contents(self::localTempFilePath, $res);
-	return get_defined_vars();
-    }
-    
-    private static function pop2021Eval(&$ref) {
-
-	$ht = $ref['res'];
-	$d = getDOMO($ht);
-	$as = $d->getElementsByTagName('a');
-	
-	$cnt = 0;
-	$is21 = false;
-	
-	foreach($as as $a) {
-	    if (!isset($a->textContent) || 
-	        strpos($a->textContent, 'Schedule & Tickets') === false) continue;
-
-	    $cnt++;
-	    $href = $a->getAttribute('href');
-	    if ($href !== 'https://www.dci.org/news/member-corps-vote-to-cancel-2020-dci-tour') $is21 = true;
-	}
-	
-	$ref['is21']       = $is21;
-	$ref['schedLinks'] = $cnt;
+    if (isset($sub)) {
+	$eret = kwynn_email::send($sub, $body, false);
     }
 }
